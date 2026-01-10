642204dcfe2f1b483718b1211791c7fc5db0e7c4
BLKNeko <blackheart2302@gmail.com>
2026-01-10 00:31:45 -0300

    cacete de drop de FPS, parece que deu bom

--- AtlyssExecuteHPIndicatorPlugin.cs
+++ AtlyssExecuteHPIndicatorPlugin.cs
@@ -1,4 +1,5 @@
 using BepInEx;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,28 +10,55 @@ namespace AtlyssExecuteHPIndicatorMod
     {
         public const string MODUID = "com.BLKNeko.AtlyssExecuteHPIndicator";
         public const string MODNAME = "com.BLKNeko.Atlyss Execute HP Indicator";
-        public const string MODVERSION = "1.0.0";
+        public const string MODVERSION = "1.0.1";
 
-        private void Update()
+        private readonly List<Image> trackedBars = new List<Image>();
+        private int scanCounter;
+
+        private void FixedUpdate()
         {
-            // percorre todas as barras de vida na cena
+            // Procura novas barras ocasionalmente
+            //if (Time.frameCount % 30 == 0) // a cada ~0.5s
+            //    ScanForHealthBars();
+
+            scanCounter++;
+
+            if (scanCounter >= 30)
+            {
+                scanCounter = 0;
+                ScanForHealthBars();
+            }
+
+            // Atualiza somente as barras conhecidas
+            for (int i = trackedBars.Count - 1; i >= 0; i--)
+            {
+                var img = trackedBars[i];
+
+                if (img == null)
+                {
+                    trackedBars.RemoveAt(i);
+                    continue;
+                }
+
+                float percent = img.fillAmount;
+                var graphic = img.GetComponent<Graphic>();
+
+                if (graphic == null)
+                    continue;
+
+                graphic.color = percent <= 0.249f
+                    ? new Color(1f, 0.9f, 0.1f)
+                    : Color.green;
+            }
+        }
+
+        private void ScanForHealthBars()
+        {
             foreach (var img in GameObject.FindObjectsOfType<Image>())
             {
-                if (img.name == "_fill_healthbar")
+                if (img.name == "_fill_healthbar" && !trackedBars.Contains(img))
                 {
-                    // porcentagem do HP (0 a 1)
-                    float percent = img.fillAmount;
-                    //Logger.LogInfo("HP: " + percent);
-
-                    var graphic = img.GetComponent<Graphic>();
-                    if (graphic == null)
-                        continue;
-
-                    // <= 20% vira amarelo
-                    if (percent <= 0.25f)
-                        graphic.color = new Color(1f, 0.9f, 0.1f); // amarelo
-                    else
-                        graphic.color = Color.green;
+                    trackedBars.Add(img);
                 }
             }
         }
